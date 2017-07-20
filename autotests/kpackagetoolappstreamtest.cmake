cd4ec70f3539f398816f6b168281c37aaec5309c
Harald Sitter <sitter@kde.org>
2017-07-20 10:03:48 +0200

    assert the testpackage appstream data validates
    Summary: helps us prevent invalid desktop->appstream conversion
    
    Test Plan: depends on D6793, with that the tests pass
    
    Reviewers: sebas, apol
    
    Reviewed By: apol
    
    Subscribers: #frameworks
    
    Tags: #frameworks
    
    Differential Revision: https://phabricator.kde.org/D6794

--- autotests/kpackagetoolappstreamtest.cmake
+++ autotests/kpackagetoolappstreamtest.cmake
@@ -7,3 +7,21 @@ execute_process(COMMAND cmake -E compare_files ${output} ${generated} ERROR_VARI
 if (error_compare)
     message(FATAL_ERROR "error on compare: ${error_compare}")
 endif()
+
+# Make sure the standard test passes appstream validation.
+get_filename_component(generated_name ${generated} NAME)
+if(${generated_name} STREQUAL "testpackage.appdata.xml")
+    find_program(APPSTREAMCLI appstreamcli)
+    if(APPSTREAMCLI)
+        execute_process(COMMAND ${APPSTREAMCLI} validate ${generated}
+            ERROR_VARIABLE appstream_stderr
+            OUTPUT_VARIABLE appstream_stdout
+            RESULT_VARIABLE result
+        )
+        if(NOT result EQUAL 0)
+            message(FATAL_ERROR "appstream data seems to be imperfect: ${appstream_stderr} ${appstream_stdout}")
+        endif()
+    else()
+        message(WARNING "skipping appstream validation as no appstreamcli binary was found")
+    endif()
+endif()
