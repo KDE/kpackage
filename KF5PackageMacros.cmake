ea92225539abb07b5957b00b931432e066c26310
Christophe Giboudeaux <christophe@krop.fr>
2018-03-28 11:09:23 +0200

    Try to fix the FreeBSD build.

--- KF5PackageMacros.cmake
+++ KF5PackageMacros.cmake
@@ -159,7 +159,7 @@ function(kpackage_install_bundled_package dir component)
     # it doesn't have OUTPUT property so it's considered out-of-date every build
     add_custom_target(${component}-${root}-contents-rcc ALL
                       COMMENT "Generating ${component}-contents.rcc"
-                      COMMAND Qt5::rcc ${kpkgqrc} --binary -o ${GENERATED_RCC_CONTENTS}
+                      COMMAND Qt5::rcc "${kpkgqrc}" --binary -o "${GENERATED_RCC_CONTENTS}"
                       DEPENDS ${component}-${root}-metadata-json ${component}-${root}-qrc)
     install(FILES ${GENERATED_RCC_CONTENTS} DESTINATION ${KDE_INSTALL_FULL_DATADIR}/${install_dir}/${root}/${component}/ RENAME contents.rcc)
 
