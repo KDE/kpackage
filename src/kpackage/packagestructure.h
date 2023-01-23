d7120fe6f99a75c25ede84d59b65bf014c070ede
Alexander Lohnau <alexander.lohnau@gmx.de>
2023-01-23 07:03:21 +0100

    Remove K_EXPORT_KPACKAGE_PACKAGE_WITH_JSON macro
    The occurrences have been ported as part of https://phabricator.kde.org/T14542

--- src/kpackage/packagestructure.h
+++ src/kpackage/packagestructure.h
@@ -112,10 +112,4 @@ private:
 
 } // KPackage namespace
 
-/**
- * Register a Package class when it is contained in a loadable module
- */
-
-#define K_EXPORT_KPACKAGE_PACKAGE_WITH_JSON(classname, jsonFile) K_PLUGIN_FACTORY_WITH_JSON(factory, jsonFile, registerPlugin<classname>();)
-
 #endif
